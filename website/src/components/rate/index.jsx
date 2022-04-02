9188e6e17350dd8fb4bea9f2fe8c643fcbea1ced
Mohamamd Raufzahed <mohamamdraufzahed@protonmail.com>
2022-04-02 16:19:49 +0430

    Added the error handler with animation

--- website/src/components/rate/index.jsx
+++ website/src/components/rate/index.jsx
@@ -4,29 +4,49 @@ import classes from "../../styles/components/Rate/index.module.scss";
 
 const Rate = ({ title, description }) => {
   const [active, setActive] = useState(0);
+  const [submited, setSubmited] = useState(false);
   return (
     <section className={classes.container}>
-      <StarIcon width="20px" className={classes.icon} />
-      <h1 className={classes.title}>{title}</h1>
-      <p className={classes.description}>{description}</p>
-      <ul className={classes.rating_list}>
-        <li active={active == 1} onClick={() => setActive(1)}>
-          1
-        </li>
-        <li active={active == 2} onClick={() => setActive(2)}>
-          2
-        </li>
-        <li active={active == 3} onClick={() => setActive(3)}>
-          3
-        </li>
-        <li active={active == 4} onClick={() => setActive(4)}>
-          4
-        </li>
-        <li active={active == 5} onClick={() => setActive(5)}>
-          5
-        </li>
-      </ul>
-      <button className={classes.button}>Submit</button>
+      {submited ? null : (
+        <>
+          <StarIcon width="20px" className={classes.icon} />
+          <h1 className={classes.title}>{title}</h1>
+          <p className={classes.description}>{description}</p>
+          <ul className={classes.rating_list}>
+            <li active={active == 1} onClick={() => setActive(1)}>
+              1
+            </li>
+            <li active={active == 2} onClick={() => setActive(2)}>
+              2
+            </li>
+            <li active={active == 3} onClick={() => setActive(3)}>
+              3
+            </li>
+            <li active={active == 4} onClick={() => setActive(4)}>
+              4
+            </li>
+            <li active={active == 5} onClick={() => setActive(5)}>
+              5
+            </li>
+          </ul>
+          <button
+            className={classes.button}
+            onClick={(el) => {
+              if (active) {
+                setSubmited(true);
+              } else {
+                el.currentTarget.classList.add(classes.faild);
+                setTimeout(() => {
+                  const button = document.querySelector("button");
+                  button.className = classes.button;
+                }, 500);
+              }
+            }}
+          >
+            Submit
+          </button>
+        </>
+      )}
     </section>
   );
 };
