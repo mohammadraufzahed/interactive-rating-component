1e4c4fd6882b1e06075141780b9254fdb37a6779
Mohamamd Raufzahed <mohamamdraufzahed@protonmail.com>
2022-04-02 16:06:41 +0430

    Added the first page

---
+++ website/src/app.jsx
@@ -0,0 +1,10 @@
+import Rate from "./components/rate";
+
+export function App() {
+  return (
+    <Rate
+      title="How did we do?"
+      description="Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!"
+    ></Rate>
+  );
+}
